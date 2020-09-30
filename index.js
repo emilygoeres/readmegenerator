b8b12b155ebc27bfead968602a5462c674f0ea68
Emily Goeres <goeres123@hotmail.com>
2020-09-30 16:58:55 -0600

    Finished questions, formatted document

--- index.js
+++ index.js
@@ -10,52 +10,62 @@ inquiree.prompt([
 
     // Question 1
     {
-        type:"input",
-        message:"What is the TITLE of your project?",
-        name:"Project Name"
+        type: "input",
+        message: "What is the TITLE of your project?",
+        name: "Project Name"
     }
 // Question 2
 {
-    type:"input",
-    message:"What is the DESCRIPTION of your project?",
-    name:"Project Description"
-}
+        type: "input",
+        message: "What is the DESCRIPTION of your project?",
+        name: "Project Description"
+    }
 // Question 3
 {
-    type:"checkbox",
-    message:"Please Add a Table of Contents",
-    name:"Project Table of Contents"
-    choices:["index.js", "package-lock.js", "package.json", "style.css" , "index.html", "images", "readMe"]
-}
+        type: "checkbox",
+        message: "Please Add a Table of Contents",
+        name: "Project Table of Contents"
+    choices: ["index.js", "package-lock.js", "package.json", "style.css", "index.html", "images", "readMe"]
+    }
 // Question 4
 {
-    type:"checkbox",
-    message:"Did you install anything?",
-    name:"Project Installations"
-    choices:[""]
-}
+        type: "checkbox",
+        message: "Did you install anything?",
+        name: "Project Installations"
+    choices: [""]
+    }
 // Question 5
 {
-    type:"input",
-    message:"What is the application used for?",
-    name:"application usage"
-}
+        type: "input",
+        message: "What is the application used for?",
+        name: "application usage"
+    }
 // Question 6
 {
-    type:"input",
-    message:"Does this application need a license?",
-    name:"application license"
-}
+        type: "input",
+        message: "Does this application need a license?",
+        name: "application license"
+    }
+// Question 7
+{
+        type: "input",
+        message: "What was your contribution?",
+        name: "contribution"
+    }
+// Question 8
+{
+        type: "input",
+        message: "Did you run any tests for your application?",
+        name: "Test Application"
+    }
+// Question 9
+{
+        type: "input",
+        message: "Did you ren into any unanswered questions?",
+        name: "Application Questions"
+    }
 ])
 
 
 
 
-
-
-
-
-* License
-* Contributing
-* Tests
-* Questions
