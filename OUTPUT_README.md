02d7966700fbf087b1fa8e67ca809a299c656ac1
Emily Goeres <goeres123@hotmail.com>
2020-09-30 20:18:41 -0600

    generated the readMe now

---
+++ OUTPUT_README.md
@@ -0,0 +1,24 @@
+
+# cheese
+------------------------
+_cheese_
+--------------------------------
+## author: cheese
+
+
+### Table Of Contents
+* [license](#license)
+* [usage](#usage)
+* [Installation](#Installation)
+* [Test](#Test)
+
+
+## license
+## usage
+cheese
+## Installation
+cheese
+## Test
+cheese
+If you have any questions regarding this project please reach me at cheese
+    
--- index.js
+++ index.js
@@ -8,71 +8,95 @@ var fs = require('fs');
 
 inquirer.prompt([
 
-// Question 1
+    // Question 1
     {
         type: "input",
         message: "What is the TITLE of your project?",
-        name: "Project Name",
+        name: "ProjectName",
     },
-// Question 2
+    // Question 2
     {
         type: "input",
         message: "What is the DESCRIPTION of your project?",
-        name: "Project Description",
+        name: "ProjectDescription",
     },
-// Question 3
+    // Question 3
     {
         type: "checkbox",
-        message: "Please Add a Table of Contents",
-        name: "Project Table of Contents",
-        choices: ["index.js", "package-lock.js", "package.json", "style.css", "index.html", "images", "readMe"],
+        message: "Choose project license",
+        name: "applicationlicense",
+        choices: ["MIT", "ISC", "Apache 2.0", "None"],
     },
-// Question 4
+    // Question 4
     {
         type: "input",
         message: "Did you install anything?",
-        name: "Project Installations",
+        name: "ProjectInstallations",
     },
-// Question 5
-    {
-        type: "input",
-        message: "What is the application used for?",
-        name: "application usage",
-    },
-// Question 6
+   
+    // Question 6
     {
         type: "input",
         message: "Does this application need a license?",
-        name: "application license",
+        name: "applicationlicense",
     },
-// Question 7
+    // Question 7
     {
         type: "input",
         message: "What was your contribution?",
         name: "contribution",
     },
-// Question 8
+    // Question 8
     {
         type: "input",
         message: "Did you run any tests for your application?",
-        name: "Test Application",
+        name: "TestApplication",
     },
-// Question 9
+    // Question 9
     {
         type: "input",
-        message: "Did you ren into any unanswered questions?",
-        name: "Application Questions",
+        message: "What is your email?",
+        name: "email",
     }
-// What to do when I get the answers
+    // What to do when I get the answers
 ]).then(function (response) {
     console.log(response)
-        // Log answers
-        // console.log answers
-        // fs.writeFile(response.name + ".json", JSON.stringify(response) (function)
-        // if (err) {
-        //     throw err
-        // }
-        // console.log()
+    var readMeText = `
+# ${response.ProjectName}
+------------------------
+_${response.ProjectDescription}_
+--------------------------------
+## author: ${response.contribution}
+
+
+### Table Of Contents
+* [license](#license)
+* [usage](#usage)
+* [Installation](#Installation)
+* [Test](#Test)
+
+
+## license
+![GitHub license](https://img.shields.io/badge/license-${response.applicationlicense}-blue.svg)
+
+
+## usage
+${response.applicationusage}
+## Installation
+${response.ProjectInstallations}
+## Test
+${response.TestApplication}
+If you have any questions regarding this project please reach me at ${response.email}
+    `
+    console.log(readMeText)
+
+    fs.writeFileSync("OUTPUT_README.md", readMeText, function (err) {
+        if (err) {
+            throw err
+        }
+        console.log("File Generator")
+    })
+        
     });
 
 
